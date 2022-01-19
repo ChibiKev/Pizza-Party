4daf6c2f38496d03087df251d4da51ca2954be25
ChibiKev <k3v.ch3n@gmail.com>
2022-01-19 15:53:32 -0500

    Added A Few More Functionality

--- app/src/main/java/com/chibikev/pizzaparty/MainActivity.java
+++ app/src/main/java/com/chibikev/pizzaparty/MainActivity.java
@@ -15,9 +15,11 @@ public class MainActivity extends AppCompatActivity {
     public final static int SLICES_PER_PIZZA = 8;
 
     private EditText mNumAttendEditText;
+    private EditText mNumAmountEditText;
     private TextView mNumPizzasTextView;
+    private TextView mCostTextView;
     private RadioGroup mHowHungryRadioGroup;
-
+    private RadioGroup mToppingsRadioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,37 +28,84 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate was called");
         // Assign the widgets to fields
         mNumAttendEditText = findViewById(R.id.num_attend_edit_text);
+        mNumAmountEditText = findViewById(R.id.num_amount_edit_text);
         mNumPizzasTextView = findViewById(R.id.num_pizzas_text_view);
+        mCostTextView = findViewById(R.id.cost_text_view);
         mHowHungryRadioGroup = findViewById(R.id.hungry_radio_group);
+        mToppingsRadioGroup = findViewById(R.id.toppings_radio_group);
     }
 
-    public void calculateClick(View view) {
+    public void calculatePizzaClick(View view) {
 
         // Get the text that was typed into the EditText
         String numAttendStr = mNumAttendEditText.getText().toString();
+        String numAmountStr = mNumAmountEditText.getText().toString();
 
         // Convert the text into an integer
         int numAttend;
+        int numAmount;
         try {
             numAttend = Integer.parseInt(numAttendStr);
         }
         catch (NumberFormatException ex) {
             numAttend = 0;
         }
-
-            // Determine how many slices on average each person will eat
-        int slicesPerPerson = 0;
-        int checkedId = mHowHungryRadioGroup.getCheckedRadioButtonId();
-        if (checkedId == R.id.light_radio_button) {
-            slicesPerPerson = 2;
-        } else if (checkedId == R.id.medium_radio_button) {
-            slicesPerPerson = 3;
-        } else if (checkedId == R.id.ravenous_radio_button) {
-            slicesPerPerson = 4;
+        try {
+            numAmount = Integer.parseInt(numAmountStr);
+        }
+        catch (NumberFormatException ex) {
+            numAmount = 1;
         }
 
         // Calculate and show the number of pizzas needed
-        int totalPizzas = (int) Math.ceil(numAttend * slicesPerPerson / (double) SLICES_PER_PIZZA);
+        int totalPizzas = (int) Math.ceil(numAmount * numAttend / (double) SLICES_PER_PIZZA);
         mNumPizzasTextView.setText("Total pizzas: " + totalPizzas);
+    }
+
+    public void calculatePriceClick(View view) {
+        // Get the text that was typed into the EditText
+        String numAttendStr = mNumAttendEditText.getText().toString();
+        String numAmountStr = mNumAmountEditText.getText().toString();
+
+        // Convert the text into an integer
+        int numAttend;
+        int numAmount;
+        try {
+            numAttend = Integer.parseInt(numAttendStr);
+        }
+        catch (NumberFormatException ex) {
+            numAttend = 0;
+        }
+        try {
+            numAmount = Integer.parseInt(numAmountStr);
+        }
+        catch (NumberFormatException ex) {
+            numAmount = 1;
+        }
+
+        int cost = 0;
+        int checkedId = mHowHungryRadioGroup.getCheckedRadioButtonId();
+        if (checkedId == R.id.small_radio_button) {
+            cost = 8;
+        } else if (checkedId == R.id.medium_radio_button) {
+            cost = 10;
+        } else if (checkedId == R.id.large_radio_button) {
+            cost = 12;
+        }
+
+        int toppingCost = 0;
+        checkedId = mToppingsRadioGroup.getCheckedRadioButtonId();
+        if (checkedId == R.id.plain_radio_button) {
+            toppingCost = 0;
+        } else if (checkedId == R.id.pepperoni_radio_button) {
+            toppingCost = 2;
+        } else if (checkedId == R.id.chicken_radio_button) {
+            toppingCost = 2;
+        }
+
+        // Calculate and show the cost of pizzas
+        int totalPizza = (int) Math.ceil(numAmount * numAttend / (double) SLICES_PER_PIZZA);
+        int totalCost = totalPizza * (cost + toppingCost);
+        mCostTextView.setText("Total Cost: " + totalCost);
     }
 }
